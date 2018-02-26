5a3c2f2b74b30f91d78ce2695bd72aad723dcd66
Carol Harris <carolharris@Carols-MacBook-Pro.local>
2018-02-26 16:43:21 +0000

    Initial Commit

---
+++ Dicee App/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Carol Harris on 26/02/2018.
+//  Copyright © 2018 Andy Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    let diceImages = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        
+        // Create randon numbers between 0 and 5 inclusive
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        // change dice images accordingly
+        diceImageView1.image = UIImage(named: diceImages[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceImages[randomDiceIndex2])
+        
+    }
+}
+
